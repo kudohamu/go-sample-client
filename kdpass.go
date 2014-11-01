49941fc734fe05cc52951a65210f0bfde4c4bd08
kudohamu <kudohamu91086@yahoo.co.jp>
2014-11-02 03:39:30 +0900

    ひとまず大枠を作成
    ・固定IP, Portで待つサーバにTCPで接続
    ・サーバにひとことだけメッセージを送る
    ・サーバからひとことだけメッセージをもらう
    ・まだ一回しか接続できない

---
+++ kdpass.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s message", os.Args[0])
+		os.Exit(1)
+	}
+	message := os.Args[1]
+
+	serverIP := "192.168.5.17"
+	serverPort := "51456"
+	myIP := "192.168.5.17"
+	myPort := 51432
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", serverIP+":"+serverPort)
+	checkError(err)
+	myAddr := new(net.TCPAddr)
+	myAddr.IP = net.ParseIP(myIP)
+	myAddr.Port = myPort
+	conn, err := net.DialTCP("tcp", myAddr, tcpAddr)
+	checkError(err)
+	defer conn.Close()
+
+	conn.Write([]byte(message))
+	checkError(err)
+
+	readBuf := make([]byte, 1024)
+	readlen, err := conn.Read(readBuf)
+	checkError(err)
+
+	fmt.Println("server: " + string(readBuf[:readlen]))
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fatal: error: %s", err.Error())
+		os.Exit(1)
+	}
+}
